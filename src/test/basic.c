cda6cb97c1ad73c8eff0507f5bd961d5d0796fd0
E-gy <balakhelisey@gmail.com>
2020-11-19 20:14:08 +0100

    Simplest "test" of em all

---
+++ src/test/basic.c
@@ -0,0 +1,15 @@
+#include "../main/gramdef.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+DEF_SYMBOL_TERMINAL(symb_eof, { return !str || !*str; })
+DEF_SYMBOL_TERMINAL(symb_a, { return streq(str, "a"); })
+DEF_SYMBOL_TERMINAL(symb_b, { return streq(str, "b"); })
+DEF_GROUP(grp_0, RULE(SYMBOL_T(symb_a); SYMBOL_T(symb_eof)); RULE(SYMBOL_T(symb_b); SYMBOL_G(grp_0)))
+DEF_GRAMMAR(grm_basic, GROUP(grp_0))
+
+int main(){
+	Grammar g = grm_basic();
+	printf("grammar: %p\n", g);
+}
