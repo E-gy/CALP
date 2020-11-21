5b7a7700bd09c569c90fd277895e5de0c22669b7
E-gy <balakhelisey@gmail.com>
2020-11-21 02:35:51 +0100

    +-/*() grammar & parsing test

---
+++ src/test/mathg.c
@@ -0,0 +1,48 @@
+#include "../main/grammaro.h"
+#include "../main/gramdef.h"
+#include "../main/parser.h"
+#include "../main/parserp.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+DEF_SYMBOL_TERMINAL(eof, { return !*str ? str : NULL; })
+DEF_SYMBOL_TERMINAL(ε, { return str; })
+DEF_SYMBOL_TERMINAL(number, {
+	if(!isdigit(*str)){
+		if(*str != '-') return NULL;
+		str++;
+		if(!isdigit(*str)) return NULL;
+	}
+	for(; isdigit(*str); str++);
+	return str;
+})
+DEF_SYMBOL_TERMINAL(plus, { return str[0] == '+' ? str+1 : NULL; })
+DEF_SYMBOL_TERMINAL(minus, { return str[0] == '-' ? str+1 : NULL; })
+DEF_SYMBOL_TERMINAL(times, { return str[0] == '*' ? str+1 : NULL; })
+DEF_SYMBOL_TERMINAL(divide, { return str[0] == '/' ? str+1 : NULL; })
+DEF_SYMBOL_TERMINAL(lpar, { return str[0] == '(' ? str+1 : NULL; })
+DEF_SYMBOL_TERMINAL(rpar, { return str[0] == ')' ? str+1 : NULL; })
+static Group ng();
+static Group muls();
+static Group adds();
+DEF_GROUP(pOm, RULE(SYMBOL_T(plus)); RULE(SYMBOL_T(minus)))
+DEF_GROUP(tOd, RULE(SYMBOL_T(times)); RULE(SYMBOL_T(divide)))
+DEF_GROUP(muls_, RULE(SYMBOL_G(tOd); SYMBOL_G(muls)); RULE(SYMBOL_T(ε)))
+DEF_GROUP(muls, RULE(SYMBOL_G(ng); SYMBOL_G(muls_)))
+DEF_GROUP(adds_, RULE(SYMBOL_G(pOm); SYMBOL_G(adds)); RULE(SYMBOL_T(ε)))
+DEF_GROUP(adds, RULE(SYMBOL_G(muls); SYMBOL_G(adds_)))
+DEF_GROUP(ng, RULE(SYMBOL_T(lpar); SYMBOL_G(adds); SYMBOL_T(rpar)); RULE(SYMBOL_T(number)))
+DEF_GRAMMAR(math, GROUP(ng); GROUP(pOm); GROUP(tOd); GROUP(muls_); GROUP(muls); GROUP(adds_); GROUP(adds);)
+
+int main(){
+	Grammar g = math();
+	printf("grammar: %p\n", g);
+	grammar_log(g);
+	Parser p = parser_build(g);
+	if(!p) return 1;
+	ast_log(parseraw(p, "12", &adds));
+	ast_log(parseraw(p, "12+25", &adds));
+	ast_log(parseraw(p, "(-21*13/2)*((12/2-25*4)-1)", &adds));
+}
