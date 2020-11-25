c737774241d6c0f3be82f4265a180475d72fc7ca
E-gy <balakhelisey@gmail.com>
2020-11-26 00:49:14 +0100

    Typed result - guarded & if else

--- src/api/calp/ptypes.h
+++ src/api/calp/ptypes.h
@@ -15,8 +15,9 @@ typedef bool Result;
 #define Error_T(T,...) ((struct T){Error, {.error=__VA_ARGS__}})
 #define IsOk_T(resultt) IsOk(resultt.result)
 #define IsNotOk_T(resultt) IsNotOk(resultt.result)
-#define IfOk_T(result, var, closure) do{ if(IsOk_T(result)){ typeof(result.r.ok) var = result.r.ok; closure } }while(0)
-#define IfError_T(result, var, closure) do{ if(IsNotOk_T(result)){ typeof(result.r.error) var = result.r.error; closure } }while(0)
+#define IfOk_T(result, var, closure) do{ typeof(result) _r = result; if(IsOk_T(_r)){ typeof(_r.r.ok) var = _r.r.ok; closure } }while(0)
+#define IfError_T(result, var, closure) do{ typeof(result) _r = result; if(IsNotOk_T(_r)){ typeof(_r.r.error) var = _r.r.error; closure } }while(0)
+#define IfElse_T(result, vok, clok, verr, clerr) do{ typeof(result) _r = result; if(IsOk_T(_r)){ typeof(_r.r.ok) vok = _r.r.ok; clok } else { typeof(_r.r.error) verr = _r.r.error; clerr } }while(0)
 
 typedef const char* string;
 typedef char* string_mut;
