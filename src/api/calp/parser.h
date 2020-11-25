3ed21ad7b44ae1403b1efe75d4bf9d33c0c70208
E-gy <balakhelisey@gmail.com>
2020-11-26 00:03:04 +0100

    Proper parser headers - h refactor (7)

--- src/api/calp/parser.h
+++ src/api/calp/parser.h
@@ -1,8 +1,4 @@
 #pragma once
 
-#include "grammar.h"
-
 struct parser;
 typedef struct parser* Parser;
-
-Parser parser_build(Grammar gr);
--- src/main/calp/parserp.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <calp/parser.h>
-#include <calp/ast.h>
-
-AST parseraw(Parser parser, string input, GroupId g0);
