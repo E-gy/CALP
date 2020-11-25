7f5fff7fdd8e043aa1ce246eab008d90621c4302
E-gy <balakhelisey@gmail.com>
2020-11-25 23:13:23 +0100

    Typed result fixes

--- src/api/calp/ptypes.h
+++ src/api/calp/ptypes.h
@@ -13,10 +13,10 @@ typedef bool Result;
 #define Result_T(T, okt, errt) struct T { Result result; union { okt ok; errt error; } r; } 
 #define Ok_T(T,...) ((struct T){Ok, {.ok=__VA_ARGS__}})
 #define Error_T(T,...) ((struct T){Error, {.error=__VA_ARGS__}})
-#define IsOk_T(result) IsOk(result.result)
-#define IsNotOk_T(result) IsNotOk(result.result)
-#define IfOk_T(result, var, closure) do{ if(IsOk_T(result){ typeof(result.ok) var = result.ok; closure } }while(0)
-#define IfError_T(result, var, closure) do{ if(IsNotOk_T(result){ typeof(result.error) var = result.error; closure } }while(0)
+#define IsOk_T(resultt) IsOk(resultt.result)
+#define IsNotOk_T(resultt) IsNotOk(resultt.result)
+#define IfOk_T(result, var, closure) do{ if(IsOk_T(result)){ typeof(result.r.ok) var = result.r.ok; closure } }while(0)
+#define IfError_T(result, var, closure) do{ if(IsNotOk_T(result)){ typeof(result.r.error) var = result.r.error; closure } }while(0)
 
 typedef const char* string;
 typedef char* string_mut;
