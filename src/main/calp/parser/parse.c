e6a2d3cf56547477838a9dd1e7de63c45db391ab
E-gy <balakhelisey@gmail.com>
2020-11-26 00:38:27 +0100

    Parser parsing with results - parser updates (1)

--- src/main/calp/parser/parse.c
+++ src/main/calp/parser/parse.c
@@ -3,23 +3,24 @@
 #include "internals.h"
 #include <calp/util/buffer.h>
 
-static AST parser_makast(Parser p, Symbol symb, string* str){
+static ParseResult parser_makast(Parser p, Lexer l, Symbol symb, string* str){
 	switch(symb->type){
 		case SYMBOL_TYPE_TERM: {
 			string nom = symb->val.term.id(*str);
 			if(!nom){
 				logdebug("'%s' failed to match \"%s\"", symb->val.term.name, *str);
-				return null;
+				return Error_T(parse_result, {"Terminal symbol failed to match"});
 			}
 			AST ret = ast_new_leaf(symb, buffer_destr(buffer_new_from(*str, nom-*str)));
+			if(!ret) return Error_T(parse_result, {"[INTERNAL] AST leaf construction failed"});
 			*str = nom;
-			return ret;
+			return Ok_T(parse_result, ret);
 		}
 		case SYMBOL_TYPE_GROUP: {
 			EntityInfo gi = entimap_get(p->ents, entinf_blank_group(symb->val.group.id));
-			if(!gi) return null; //internal error
+			if(!gi) return Error_T(parse_result, {"[INTERNAL] Invalid state: group not in map"});
 			for(FirstListElement fl = gi->i.group.firsts->first; fl; fl = fl->next){
-				if(fl->symbol->type != SYMB_TERM) return null; //internal error
+				if(fl->symbol->type != SYMB_TERM) return Error_T(parse_result, {"[INTERNAL] Invalid state: non-terminal first list element"});
 				if(fl->symbol->i.term.symbolId(*str)){
 					Rule r = fl->r;
 					size_t rsc = 0;
@@ -28,14 +29,14 @@ static AST parser_makast(Parser p, Symbol symb, string* str){
 					size_t i = 0;
 					string sstr = *str;
 					for(Symbol rs = r->symbols; rs; rs = rs->next){
-						AST rsast = parser_makast(p, rs, &sstr);
-						if(!rsast) break;
-						gast->d.group.children[i++] = rsast;
+						ParseResult rsast = parser_makast(p, l, rs, &sstr);
+						if(!IsOk_T(rsast)) break;
+						gast->d.group.children[i++] = rsast.r.ok;
 					}
 					if(i != rsc) ast_destroy(gast);
 					else {
 						*str = sstr;
-						return gast;
+						return Ok_T(parse_result, gast);
 					}
 				}
 			}
@@ -47,30 +48,30 @@ static AST parser_makast(Parser p, Symbol symb, string* str){
 				size_t i = 0;
 				string sstr = *str;
 				for(Symbol rs = r->symbols; rs; rs = rs->next){
-					AST rsast = parser_makast(p, rs, &sstr);
-					if(!rsast) break;
-					gast->d.group.children[i++] = rsast;
+					ParseResult rsast = parser_makast(p, l, rs, &sstr);
+					if(!IsOk_T(rsast)) break;
+					gast->d.group.children[i++] = rsast.r.ok;
 				}
 				if(i != rsc) ast_destroy(gast);
 				else {
 					*str = sstr;
-					return gast;
+					return Ok_T(parse_result, gast);
 				}
 			}
 			logdebug("<%s> first list exhausted, no matches", gi->i.group.group->name);
-			return null; //parser error
+			return Error_T(parse_result, "First list exhausted, no matches found");
 		}
-		default: return null; //._.
+		default: return Error_T(parse_result, "._."); //._.
 	}
 } 
 
 #include <calp/grammar/build.h>
 
-AST parseraw(Parser p, string s, GroupId g0){
-	if(!p || !s || !g0) return null;
+ParseResult parser_parse(Parser p, Lexer l, string s, GroupId g0){
+	if(!p || !l || !s || !g0) return Error_T(parse_result, {"Invalid args"});
 	Symbol gsymb = symbol_new_group(g0);
-	if(!gsymb) return null;
-	AST ast = parser_makast(p, gsymb, &s);
+	if(!gsymb) return Error_T(parse_result, {"Allocation failed"});
+	ParseResult result = parser_makast(p, l, gsymb, &s);
 	symbol_destroy(gsymb);
-	return ast;
+	return result;
 }
