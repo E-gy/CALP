017ec24c36251fb2f2646fdec5973144fd16dee1
E-gy <balakhelisey@gmail.com>
2020-12-12 01:38:14 +0100

    not in entity info after all

--- src/main/calp/parser/internals.h
+++ src/main/calp/parser/internals.h
@@ -63,7 +63,6 @@ struct entinf {
 	} type;
 	bool init;
 	bool nullable;
-	int priority;
 	union {
 		struct {
 			/** @ref */ TerminalSymbolId symbolId;
@@ -81,9 +80,9 @@ struct entinf {
 	EntityInfo mapnext;
 };
 
-#define entinf_blank_term(s) ((struct entinf){SYMB_TERM, false, false, 0, {.term = {s, null}}, null})
-#define entinf_blank_group(g) ((struct entinf){SYMB_GROUP, false, false, 0, {.group = {g, null, null}}, null})
-#define entinf_blank_rule(r) ((struct entinf){RULE, false, false, 0, {.rule = {r}}, null})
+#define entinf_blank_term(s) ((struct entinf){SYMB_TERM, false, false, {.term = {s, null}}, null})
+#define entinf_blank_group(g) ((struct entinf){SYMB_GROUP, false, false, {.group = {g, null, null}}, null})
+#define entinf_blank_rule(r) ((struct entinf){RULE, false, false, {.rule = {r}}, null})
 
 #define entinf_blank_symbol(_s) (_s ? _s->type == SYMBOL_TYPE_TERM ? entinf_blank_term(_s->val.term.id) : _s->type == SYMBOL_TYPE_GROUP ? entinf_blank_group(_s->val.group.id) : entinf_blank_rule(null) : entinf_blank_rule(null))
 
